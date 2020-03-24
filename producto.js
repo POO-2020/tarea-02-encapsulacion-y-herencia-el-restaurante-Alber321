af139bd7a32365292a0f3446757b48fa4ebe473d
Alber321 <lsolis2@ucol.mx>
2020-03-24 17:11:22 -0600

    Producto

---
+++ producto.js
@@ -0,0 +1,10 @@
+export default class Producto {
+    constructor(nombre,precio)
+    {
+        this._nombre = nombre
+        this._precio = precio
+    }
+    getDescripcion(){
+        return (`${this._nombre}, ${this._precio.getPrecio()}`)
+    }
+}
