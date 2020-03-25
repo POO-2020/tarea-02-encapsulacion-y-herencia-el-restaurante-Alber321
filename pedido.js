b19630c16ed9f82fc669f0d465b3f1d16fd23523
Alber321 <lsolis2@ucol.mx>
2020-03-24 18:26:30 -0600

    Pedido

---
+++ pedido.js
@@ -0,0 +1,53 @@
+import Precio from "./precio.js"
+export default class Pedido {
+    constructor({fecha,hora,cliente, NumeroP}){
+        this._fecha = fecha
+        this._hora = hora 
+        this._cliente = cliente
+        this._Numerop = Numerop;
+        this._elep = []
+    }
+    agregarElementos(elemento){
+        this._elep.push(elemento)
+    }
+    listarElementos(){
+        console.log ("Elementos: ")
+        this.elep.forEach((ele, i) => {
+            console.log (`(${i + 1}) --- ${ele.getDescripcion()}`);
+        });
+    }
+    getCostoTotal(){
+        let total = 0
+        this.elep.forEach((ele) => {
+            total = (total + (ele.producto.precio.valor * ele.cantidad))
+        })
+        total = new Precio (total)
+        return total.getPrecio()
+    }
+    getProductos(){
+        let productos = 0
+        this.elep.forEach((ele, i) => {
+            productos = productos + ele.cantidad
+        })
+        return productos;
+    }
+    getNumeroElementos(){
+        let numEl = 0
+        this.elep.forEach((ele, i) => {
+            numEl = numEl + 1
+        })
+        return numEl;
+    }
+    getResumen(){
+        return`${this.fecha.getFecha()} ${this.hora.getFormato12()} - ${this.getNumeroElementos()} 
+        elementos con ${this.getProductos()} productos - total: ${this.getCostoTotal()}`
+    }
+    getNumeroPedido(){
+        return this._Numerop
+    }
+
+    Igual(pedido){
+        if(pedido.getNumeroPedido() == this._Numerop){ return true}
+        else {return false}
+    }
+}
